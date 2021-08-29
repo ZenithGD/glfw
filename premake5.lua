37e14d7ceebb2d6f6f31d548499a455d40380592
ZenithGD <darimarcos456@hotmail.com>
2021-08-30 01:53:40 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -10,64 +10,67 @@ project "GLFW"
 
     files {
         "include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c",
-
-        "src/egl_context.c",
-        "src/osmesa_context.c"
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c",
     }
 
     filter "system:windows"
-		systemversion "latest"
+        systemversion "latest"
 
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-		}
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
+        defines 
+        { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
 
     filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/linux_joystick.c"
-		}
+        pic "On"
+        systemversion "latest"
+        
+        files
+        {
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/glx_context.c",
+            "src/linux_joystick.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
 
-		defines
-		{
-			"_GLFW_X11"
-		}
+        defines
+        {
+            "_GLFW_X11"
+        }
 
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
+    filter "configurations:Debug"
+        runtime "Debug"
+        buildoptions "/MTd"
+        symbols "on"
 
-        filter "configurations:Debug"
-            runtime "Debug"
-            symbols "on"
-
-        filter "configurations:Release"
-            runtime "Release"
-            optimize "on"
+    filter "configurations:Release"
+        runtime "Release"
+        buildoptions "/MT"
+        optimize "on"
